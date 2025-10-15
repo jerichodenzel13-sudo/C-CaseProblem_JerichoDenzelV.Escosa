d0e67d0491dbc70133c642af8538f465bf515e6e
jerichodenzel13-sudo <jerichodenzel13@gmail.com>
2025-10-15 15:06:58 +0800

    Add files via upload
    Uploaded C case problem and output screenshot.

---
+++ Escosa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+      
+      printf("Name: Jericho Denzel V. Escosa\n");
+      printf("ID Number: 2025303963\n");
+      printf("------------------------\n");
+      
+      
+      int num1 = 2, num2 = 3, sum;
+      
+      sum = num1 + num2;
+      
+      printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+      
+      return 0;
+      
+  }
